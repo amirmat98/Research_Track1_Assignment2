0dcd5452564e36f537f8a059630d8251a09b1d63
amirmat98 <matin.amirmahdi@gmail.com>
2024-05-07 16:57:56 +0200

    adjust conf file

--- source/conf.py
+++ source/conf.py
@@ -11,18 +11,39 @@ copyright = '2024, Amirmahdi Matin'
 author = 'Amirmahdi Matin'
 release = '1.0'
 
+import os
+import sys
+sys.path.insert(0, os.path.dirname('../'))
+
+show_author = True
+
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+'sphinx.ext.autodoc',
+'sphinx.ext.doctest',
+'sphinx.ext.intersphinx',
+'sphinx.ext.todo',
+'sphinx.ext.coverage',
+'sphinx.ext.mathjax',
+'sphinx.ext.ifconfig',
+'sphinx.ext.viewcode',
+'sphinx.ext.githubpages',
+"sphinx.ext.napoleon",
+'sphinx.ext.inheritance_diagram',
+'breathe'
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
+source_suffix = '.rst'
+master_doc = 'index'
+html_theme = 'sphinx_rtd_theme'
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
 html_static_path = ['_static']
