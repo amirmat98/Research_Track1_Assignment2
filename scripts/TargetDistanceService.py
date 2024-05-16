ce742211707539aca227c4577c7288bdca107469
amirmat98 <matin.amirmahdi@gmail.com>
2024-05-16 15:52:43 +0200

    Compleate docstrings

--- scripts/TargetDistanceService.py
+++ scripts/TargetDistanceService.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
 
+"""
+.. module:: TargetDistanceService
+   :platform: Unix
+   :synopsis: Node for calculating the relative position and velocity of the robot to its target in the RT1_Second_Assignment project.
+
+.. moduleauthor:: Your Name Here
+
+This module implements a ROS node that calculates the distance and average velocity of a robot relative to a specified target position. The node subscribes to both the robot's current position and velocity and a target position, and provides a service that computes the difference in position and average velocity.
+"""
+
 import rospy
 from rt1_a2_2023.msg import RobotPositionVelocity, TargetPosition
 from rt1_a2_2023.srv import RobotToTarget, RobotToTargetResponse
@@ -13,12 +23,24 @@ Velocities = []
 
 
 def TargetPosCallback(data):
+    """
+    Callback for target position updates.
+
+    :param data: New target position data.
+    :type data: TargetPosition
+    """
     global TargetFlag, Target
     Target.x = data.x
     Target.y = data.y
     TargetFlag = True
 
 def RobotPosVeCallback(data):
+    """
+    Callback for robot position and velocity updates.
+
+    :param data: New robot position and velocity data.
+    :type data: RobotPositionVelocity
+    """
     global Velocities
     Robot.x_pos = data.x
     Robot.y_pos = data.y
@@ -29,6 +51,14 @@ def RobotPosVeCallback(data):
         Velocities.append((data.v_x, data.v_y))
 
 def RobotToTargetCallback(req):
+    """
+    Service callback to calculate and provide the distance and average velocity from the robot to the target.
+
+    :param req: The service request (empty for this service)
+    :type req: RobotToTargetRequest
+    :returns: Distance and average velocity from the robot to the target.
+    :rtype: RobotToTargetResponse
+    """
     response = RobotToTargetResponse()
     global TargetFlag, Target, Robot, Velocities
 
@@ -51,6 +81,9 @@ def RobotToTargetCallback(req):
 
 
 def main():
+    """
+    Main function to initialize the ROS node and its subscriptions and services.
+    """
     rospy.init_node('RobotToTarget', anonymous=False)
     rospy.Subscriber("/TargetPosition", TargetPosition, TargetPosCallback)
     rospy.Subscriber("/RobotPositionVelocity", RobotPositionVelocity, RobotPosVeCallback)
--- scripts/TargetPositionService.py
+++ scripts/TargetPositionService.py
@@ -1,4 +1,13 @@
 #! /usr/bin/env python3
+"""
+.. module:: TargetPositionService
+   :platform: Unix
+   :synopsis: Tracking and reporting the final objective location of the robot as part of the RT1_Second_Assignment project using a node.
+
+.. moduleauthor:: Amirmahdi Matin
+
+The last known target position received by the robot is returned via a service provided by a ROS node implemented in this module. The last position received by the node, which is maintained and queried via a ROS service, is subscribed to for target position updates.
+"""
 
 import rospy
 from rt1_a2_2023.msg import TargetPosition
@@ -7,11 +16,25 @@ from rt1_a2_2023.srv import LastTarget, LastTargetResponse
 PreviousTarget = None
 
 def callback (TempData):
+    """
+    Callback function for the TargetPosition subscriber.
+
+    :param TempData: the last position data received
+    :type TempData: TargetPosition
+    """
     rospy.loginfo("Last Robot Target Position has been Received")
     global PreviousTarget
     PreviousTarget = TempData
 
 def PreviousTargetImplementation(req):
+    """
+    Service handler to provide the last known target position.
+
+    :param req: The request object (empty for this service)
+    :type req: LastTargetRequest
+    :returns: The last known target position, if available
+    :rtype: LastTargetResponse
+    """
     res = LastTargetResponse()
     if PreviousTarget:
         res.x = PreviousTarget.x
@@ -21,6 +44,9 @@ def PreviousTargetImplementation(req):
     return res
 
 def main():
+    """
+    Main function to initialize the ROS node and service.
+    """
     rospy.init_node('LastTarget')
     rospy.Subscriber("/TargetPosition", TargetPosition, callback)
     rospy.Service('LastTarget', LastTarget, PreviousTargetImplementation)
